94f8fc1c55b20a8b0227e05a87ecbb2f006231b0
ezdar <ezdar2743@gmail.com>
2022-07-16 00:57:39 +0900

    test colorChange Button

--- react-jest/src/App.test.js
+++ react-jest/src/App.test.js
@@ -1,10 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color", () => {
   render(<App />);
-  const colorBtn = screen.getByRole("button", { name: "Change to Blue" });
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
   expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
 });
 
-test("button change blue when clicked", () => {});
+test("button change blue when clicked", () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  //click
+  fireEvent.click(colorBtn);
+
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorBtn.textContent).toBe("Change to red");
+});
+
+test("initial condition", () => {
+  //check that the button starts out enabled
+});
--- react-jest/src/App.tsx
+++ react-jest/src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const App = () => {
+  const [colorBtn, setColorBtn] = useState("red");
+  const isColor = colorBtn === "red" ? "blue" : "red";
+  const changeColor = () => {
+    setColorBtn(isColor);
+  };
+  console.log(colorBtn);
   return (
     <>
-      <button style={{ backgroundColor: "red" }}>Change to Blue</button>
+      <button onClick={changeColor} style={{ backgroundColor: colorBtn }}>
+        Change to {isColor}
+      </button>
     </>
   );
 };
