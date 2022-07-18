6e96a3ee74cfde3ab61faad7b50050c5a05bb557
ezdar <ezdar2743@gmail.com>
2022-07-18 12:15:28 +0900

    set up for start

--- react-jest/src/App.tsx
+++ react-jest/src/App.tsx
@@ -1,38 +1,7 @@
-import { useState } from "react";
+import React from "react";
 
-export const replaceCamelWithSpace = (colorName: string) => {
-  return colorName.replace(/\B([A-Z])\B/g, " $1");
-};
-
-console.log(replaceCamelWithSpace("RedSpdidi"));
 const App = () => {
-  const [colorBtn, setColorBtn] = useState("red");
-
-  const [disabled, setDisabled] = useState(false);
-  const isColor = colorBtn === "red" ? "blue" : "red";
-  const changeColor = () => {
-    setColorBtn(isColor);
-  };
-
-  return (
-    <>
-      <button
-        onClick={changeColor}
-        style={{ backgroundColor: disabled ? "gray" : colorBtn }}
-        disabled={disabled}
-      >
-        Change to {isColor}
-      </button>
-      <label htmlFor="disabled button">disabled button</label>
-      <input
-        type={"checkbox"}
-        defaultChecked={disabled}
-        aria-checked={disabled}
-        onChange={(e) => setDisabled(e.target.checked)}
-        id="disabled button"
-      ></input>
-    </>
-  );
+  return <div>hello</div>;
 };
 
 export default App;
