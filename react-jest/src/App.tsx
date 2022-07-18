b5df4ef7b3a2bd49f3e69bde54e11f09ca1c20d1
ezdar <ezdar2743@gmail.com>
2022-07-18 23:20:32 +0900

    Test checkbox and button

--- react-jest/src/App.tsx
+++ react-jest/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SummaryForm from "./pages/summary/SummaryForm";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      <SummaryForm />
+    </div>
+  );
 };
 
 export default App;
