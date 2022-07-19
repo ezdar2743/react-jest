c9c99efc8356cfd885bf05d8c96ef24b5d0471fb
ezdar <ezdar2743@gmail.com>
2022-07-19 11:58:46 +0900

    Test popover

--- react-jest/src/pages/summary/test/SummaryForm.test.jsx
+++ react-jest/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
-
+import userEvent from "@testing-library/user-event";
 test("initial conditions", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
@@ -12,14 +16,35 @@ test("initial conditions", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
-  fireEvent.click(checkbox);
+
+  await userEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
-  fireEvent.click(checkbox);
+  await userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+  //popover apears upon mouseover of checkbox label
+  const termsAndCondition = screen.getByText(/terms and conditions/i);
+  await userEvent.hover(termsAndCondition);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  //popover disappears when we mopuse out
+  await userEvent.unhover(termsAndCondition);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
